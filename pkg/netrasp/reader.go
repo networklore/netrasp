116372e887c8caad7afd944293258e8af9c0f6b1
Roman Dodin <dodin.roman@gmail.com>
2021-04-16 10:12:37 +0200

    modified reader

--- pkg/netrasp/reader.go
+++ pkg/netrasp/reader.go
@@ -9,7 +9,6 @@ import (
 	"strings"
 )
 
-var errContextTimeout = errors.New("context timeout")
 var errRead = errors.New("reader error")
 
 type contextReader struct {
@@ -17,13 +16,35 @@ type contextReader struct {
 	r   io.Reader
 }
 
+type readResult struct {
+	n   int
+	err error
+}
+
 func (c *contextReader) Read(p []byte) (n int, err error) {
+	ctx, cancel := context.WithCancel(c.ctx)
+	defer cancel()
+	rrCh := make(chan *readResult)
+
+	go func() {
+		select {
+		case rrCh <- c.read(p):
+		case <-ctx.Done():
+		}
+	}()
+
 	select {
-	case <-c.ctx.Done():
-		return 0, c.ctx.Err()
-	default:
-		return c.r.Read(p)
+	case <-ctx.Done():
+		return 0, ctx.Err()
+	case rr := <-rrCh:
+		return rr.n, rr.err
 	}
+}
+
+func (c *contextReader) read(p []byte) *readResult {
+	n, err := c.r.Read(p)
+
+	return &readResult{n, err}
 }
 
 func newContextReader(ctx context.Context, r io.Reader) io.Reader {
@@ -36,45 +57,27 @@ func newContextReader(ctx context.Context, r io.Reader) io.Reader {
 // readUntilPrompt reads until the specified prompt is found and returns the read data.
 func readUntilPrompt(ctx context.Context, r io.Reader, prompt *regexp.Regexp) (string, error) {
 	var output string
-	bufCh := make(chan string)
-	errCh := make(chan error)
+	r = newContextReader(ctx, r)
+	for {
+		buffer := make([]byte, 10000)
 
-	go func() {
-		for {
-			select {
-			case <-ctx.Done():
-				return
-			default:
-				buffer := make([]byte, 10000)
-
-				bytes, err := r.Read(buffer)
-				if err != nil {
-					errCh <- fmt.Errorf("error reading output from device %w: %v", errRead, err)
-				}
-				latestOutput := string(buffer[:bytes])
-
-				output += latestOutput
-
-				workingOutput := output
-				workingOutput = strings.ReplaceAll(workingOutput, "\r\n", "\n")
-				workingOutput = strings.ReplaceAll(workingOutput, "\r", "\n")
-				lines := strings.Split(workingOutput, "\n")
-				matches := prompt.FindStringSubmatch(lines[len(lines)-1])
-				if len(matches) != 0 {
-					bufCh <- output
-
-					return
-				}
-			}
+		bytes, err := r.Read(buffer)
+		if err != nil {
+			return "", fmt.Errorf("error reading output from device %w: %v", errRead, err)
 		}
-	}()
+		latestOutput := string(buffer[:bytes])
 
-	select {
-	case out := <-bufCh:
-		return out, nil
-	case err := <-errCh:
-		return "", err
-	case <-ctx.Done():
-		return "", errContextTimeout
+		output += latestOutput
+
+		workingOutput := output
+		workingOutput = strings.ReplaceAll(workingOutput, "\r\n", "\n")
+		workingOutput = strings.ReplaceAll(workingOutput, "\r", "\n")
+		lines := strings.Split(workingOutput, "\n")
+		matches := prompt.FindStringSubmatch(lines[len(lines)-1])
+		if len(matches) != 0 {
+			break
+		}
 	}
+
+	return output, nil
 }
