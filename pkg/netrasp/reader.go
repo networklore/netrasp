c8605d55d074ca886edc4acbce00ecefa90bc024
Karim Radhouani <medkarimrdi@gmail.com>
2021-04-19 18:33:56 +0800

    optimize readUntilPrompt

--- pkg/netrasp/reader.go
+++ pkg/netrasp/reader.go
@@ -1,6 +1,7 @@
 package netrasp
 
 import (
+	"bytes"
 	"context"
 	"errors"
 	"fmt"
@@ -10,6 +11,10 @@ import (
 )
 
 var errRead = errors.New("reader error")
+var repl = strings.NewReplacer("\r\n", "\n", "\r", "\n")
+
+var minReadSize = 10000
+var maxReadSize = 10 * 1000 * 1000
 
 type contextReader struct {
 	ctx context.Context
@@ -55,7 +60,7 @@ func newContextReader(ctx context.Context, r io.Reader) io.Reader {
 }
 
 // readUntilPrompt reads until the specified prompt is found and returns the read data.
-func readUntilPrompt(ctx context.Context, r io.Reader, prompt *regexp.Regexp) (string, error) {
+func readUntilPromptOriginal(ctx context.Context, r io.Reader, prompt *regexp.Regexp) (string, error) {
 	var output string
 	r = newContextReader(ctx, r)
 	for {
@@ -80,4 +85,55 @@ func readUntilPrompt(ctx context.Context, r io.Reader, prompt *regexp.Regexp) (s
 	}
 
 	return output, nil
+}
+
+// readUntilPrompt reads until the specified prompt is found and returns the read data.
+func readUntilPromptWithStringsBuilder(ctx context.Context, r io.Reader, prompt *regexp.Regexp) (string, error) {
+	var output = strings.Builder{}
+	rc := newContextReader(ctx, r)
+	readSize := minReadSize
+	for {
+		buffer := make([]byte, readSize)
+
+		n, err := rc.Read(buffer)
+		if err != nil {
+			return "", fmt.Errorf("error reading output from device %w: %v", errRead, err)
+		}
+		if readSize == n && readSize < maxReadSize {
+			readSize *= 2
+		}
+		output.Write(buffer[:n])
+		workingOutput := repl.Replace(output.String())
+		if prompt.MatchString(workingOutput[strings.LastIndex(workingOutput, "\n")+1:]) {
+			break
+		}
+	}
+
+	return output.String(), nil
+}
+
+// readUntilPrompt reads until the specified prompt is found and returns the read data.
+func readUntilPrompt(ctx context.Context, r io.Reader, prompt *regexp.Regexp) (string, error) {
+	var output = new(bytes.Buffer)
+	rc := newContextReader(ctx, r)
+	readSize := minReadSize
+	for {
+		b := make([]byte, readSize)
+
+		n, err := rc.Read(b)
+		if err != nil {
+			return "", fmt.Errorf("error reading output from device %w: %v", errRead, err)
+		}
+		if readSize == n && readSize < maxReadSize {
+			readSize *= 2
+		}
+		output.Write(b[:n])
+		tempSlice := bytes.ReplaceAll(output.Bytes(), []byte("\r\n"), []byte("\n"))
+		tempSlice = bytes.ReplaceAll(tempSlice, []byte("\r"), []byte("\n"))
+		if prompt.Match(tempSlice[bytes.LastIndex(tempSlice, []byte("\n"))+1:]) {
+			break
+		}
+	}
+
+	return output.String(), nil
 }
--- pkg/netrasp/reader_test.go
+++ pkg/netrasp/reader_test.go
@@ -11,6 +11,8 @@ var testReaderBasic = `
 here is your string
 router1#`
 
+var ns string
+
 func TestReader(t *testing.T) {
 	r := strings.NewReader(testReaderBasic)
 
@@ -32,4 +34,111 @@ func TestReaderTimeout(t *testing.T) {
 	if err == nil {
 		t.Fatalf("expected reader to timeout, but got %v", out)
 	}
+}
+
+func generatePrompt(lines int) string {
+	return strings.Repeat(strings.Repeat("dummy", 10)+"\r\r\n", lines-1) + "prompt#"
+}
+
+func runBenchmarkReaderOriginalNLines(b *testing.B, i int) {
+	b.Helper()
+	var s string
+	var err error
+
+	b.ReportAllocs()
+	for n := 0; n < b.N; n++ {
+		r := strings.NewReader(generatePrompt(i))
+		s, err = readUntilPromptOriginal(context.Background(), r, generalPrompt)
+		if err != nil {
+			b.Fail()
+		}
+	}
+	ns = s
+}
+
+func runBenchmarkReaderStringsBuilderNLines(b *testing.B, i int) {
+	b.Helper()
+	var s string
+	var err error
+	b.ReportAllocs()
+	for n := 0; n < b.N; n++ {
+		r := strings.NewReader(generatePrompt(i))
+		s, err = readUntilPromptWithStringsBuilder(context.Background(), r, generalPrompt)
+		if err != nil {
+			b.Fail()
+		}
+	}
+	ns = s
+}
+
+func runBenchmarkReaderBytesBufferNLines(b *testing.B, i int) {
+	b.Helper()
+	var s string
+	var err error
+	b.ReportAllocs()
+	for n := 0; n < b.N; n++ {
+		r := strings.NewReader(generatePrompt(i))
+		s, err = readUntilPrompt(context.Background(), r, generalPrompt)
+		if err != nil {
+			b.Fail()
+		}
+	}
+	ns = s
+}
+
+// 1K lines.
+func BenchmarkReaderOriginal100Lines(b *testing.B) {
+	runBenchmarkReaderOriginalNLines(b, 100)
+}
+func BenchmarkReaderStringsBuilder100Lines(b *testing.B) {
+	runBenchmarkReaderStringsBuilderNLines(b, 100)
+}
+func BenchmarkReaderBytesBuffer100Lines(b *testing.B) {
+	runBenchmarkReaderBytesBufferNLines(b, 100)
+}
+
+// 1K lines.
+func BenchmarkReaderOriginal1KLines(b *testing.B) {
+	runBenchmarkReaderOriginalNLines(b, 1000)
+}
+func BenchmarkReaderStringsBuilder1KLines(b *testing.B) {
+	runBenchmarkReaderStringsBuilderNLines(b, 1000)
+}
+func BenchmarkReaderBytesBuffer1KLines(b *testing.B) {
+	runBenchmarkReaderBytesBufferNLines(b, 1000)
+}
+
+// 10K lines.
+func BenchmarkReaderOriginal10KLines(b *testing.B) {
+	runBenchmarkReaderOriginalNLines(b, 10000)
+}
+func BenchmarkReaderStringsBuilder10KLines(b *testing.B) {
+	runBenchmarkReaderStringsBuilderNLines(b, 10000)
+}
+func BenchmarkReaderBytesBuffer10KLines(b *testing.B) {
+	runBenchmarkReaderBytesBufferNLines(b, 10000)
+}
+
+// 50k Lines.
+func BenchmarkReaderOriginal50KLines(b *testing.B) {
+	runBenchmarkReaderOriginalNLines(b, 50000)
+}
+func BenchmarkReaderStringsBuilder50KLines(b *testing.B) {
+	runBenchmarkReaderStringsBuilderNLines(b, 50000)
+}
+func BenchmarkReaderBytesBuffer50KLines(b *testing.B) {
+	runBenchmarkReaderBytesBufferNLines(b, 50000)
+}
+
+// 100k Lines.
+func BenchmarkReaderOriginal100KLines(b *testing.B) {
+	runBenchmarkReaderOriginalNLines(b, 100000)
+}
+
+func BenchmarkReaderStringsBuilder100KLines(b *testing.B) {
+	runBenchmarkReaderStringsBuilderNLines(b, 100000)
+}
+
+func BenchmarkReaderBytesBuffer100KLines(b *testing.B) {
+	runBenchmarkReaderBytesBufferNLines(b, 100000)
 }
